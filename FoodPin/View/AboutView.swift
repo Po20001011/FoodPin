e9afd5800e698a05e8c7d51ccfe664fea7a01d5e
Po <qaws3588@gmail.com>
2023-11-17 15:33:55 +1100

    First Commit

---
+++ FoodPin/View/AboutView.swift
@@ -0,0 +1,81 @@
+//
+//  AboutView.swift
+//  FoodPin
+//
+//  
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    
+    enum WebLink: String, Identifiable {
+        case rateUs = "https://www.apple.com/ios/app-store"
+        case feedback = "https://www.appcoda.com/contact"
+        case twitter = "https://www.twitter.com/appcodamobile"
+        case facebook = "https://www.facebook.com/appcodamobile"
+        case instagram = "https://www.instagram.com/appcodadotcom"
+        
+        var id: UUID {
+            UUID()
+        }
+    }
+    
+    @State private var link: WebLink?
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                Image("about")
+                    .resizable()
+                    .scaledToFit()
+                
+                Section {
+                    Link(destination: URL(string: WebLink.rateUs.rawValue)!, label: {
+                        Label(String(localized: "Rate us on App Store", comment: "Rate us on App Store"), image: "store")
+                            .foregroundColor(.primary)
+                    })
+                    
+                    Label(String(localized: "Tell us your feedback", comment: "Tell us your feedback"), image: "chat")
+                        .onTapGesture {
+                            link = .feedback
+                        }
+                }
+
+                Section {
+                    Label(String(localized: "Twitter", comment: "Twitter"), image: "twitter")
+                        .onTapGesture {
+                            link = .twitter
+                        }
+                    
+                    Label(String(localized: "Facebook", comment: "Facebook"), image: "facebook")
+                        .onTapGesture {
+                            link = .facebook
+                        }
+                    
+                    Label(String(localized: "Instagram", comment: "Instagram"), image: "instagram")
+                        .onTapGesture {
+                            link = .instagram
+                        }
+                }
+                
+            }
+            .listStyle(GroupedListStyle())
+            .sheet(item: $link) { item in
+                if let url = URL(string: item.rawValue) {
+                    SafariView(url: url)
+                }
+            }
+            
+            .navigationTitle("About")
+            .navigationBarTitleDisplayMode(.automatic)
+            
+        }
+    }
+}
+
+struct AboutView_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutView()
+    }
+}
---
+++ FoodPin/View/MainView.swift
@@ -0,0 +1,51 @@
+//
+//  MainView.swift
+//  FoodPin
+//
+//
+//
+
+import SwiftUI
+
+struct MainView: View {
+    
+    @State private var selectedTabIndex = 0
+    
+    var body: some View {
+        TabView(selection: $selectedTabIndex) {
+            RestaurantListView()
+                .tabItem {
+                    Label("Favorites", systemImage: "tag.fill")
+                }
+                .tag(0)
+            
+            DiscoverView()
+                .tabItem {
+                    Label("Discover", systemImage: "wand.and.rays")
+                }
+                .tag(1)
+            
+            AboutView()
+                .tabItem {
+                    Label("About", systemImage: "square.stack")
+                }
+                .tag(2)
+        }
+        .accentColor(Color("NavigationBarTitle"))
+        .onOpenURL(perform: { url in
+            
+            switch url.path {
+            case "/OpenFavorites": selectedTabIndex = 0
+            case "/OpenDiscover": selectedTabIndex = 1
+            case "/NewRestaurant": selectedTabIndex = 0
+            default: return
+            }
+        })
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
